6ca58b007756fa2299bfa03f3b60875552ed72ca
Rafael Companhoni <rafael.companhoni@axispoint.com>
2017-08-25 15:49:58 +0000

    BCOP-1663/1742: FE - hook new APIs with FE
    git-tfs-id: [http://devvm01tfs03.telamericamedia.com:8080/tfs/CMWPA]$/Telamerica/Broadcast/OnDeck;C20663

--- Source/Tam/Maestro/UI/BroadcastComposerWeb/Scripts/appTraffic/TrafficConfig.js
+++ Source/Tam/Maestro/UI/BroadcastComposerWeb/Scripts/appTraffic/TrafficConfig.js
@@ -3,11 +3,22 @@
         var gridCfg = {
             name: 'TrafficGrid',
             columns: [
-                { field: 'ID', caption: 'ID', size: '25%', sortable: true, searchable: true },
-                { field: 'Name', caption: 'Name', size: '25%', sortable: true, searchable: true },
-                { field: 'OmUnassignedIscis', caption: 'OM Unassigned ISCIs', size: '25%', sortable: true, searchable: true },
-                { field: 'Flight', caption: 'Flight', size: '25%', sortable: true, searchable: true }
-            ]
+                { field: 'Id', caption: 'ID', size: '25%' },
+                { field: 'Name', caption: 'Name', size: '25%' },
+                { field: 'Advertiser', caption: 'Advertiser', size: '25%' },
+                { field: 'OpenMarketUnassignedISCI', caption: 'OM Unassigned ISCIs', size: '25%' },
+                { field: 'ProprietaryUnassignedISCI', caption: 'Proprietary Inventory Unassigned ISCIs', size: '25%' },
+                { field: 'Flight', caption: 'Flight', size: '25%' }
+            ],
+
+            // disable nested grid toggle
+            onResize: function (event) {
+                for (var i = 0; i < this.records.length; i++) {
+                    this.expand(this.records[i].recid);
+                }
+
+                $('.w2ui-icon-collapse').remove();
+            }
         };
 
         return gridCfg;
--- Source/Tam/Maestro/UI/BroadcastComposerWeb/Scripts/appTraffic/TrafficController.js
+++ Source/Tam/Maestro/UI/BroadcastComposerWeb/Scripts/appTraffic/TrafficController.js
@@ -7,5 +7,20 @@
 
         $scope.trafficView = new TrafficView();
         $scope.trafficView.initView(this);
+    },
+
+    apiGetTraffic: function (callback, errorCallback) {
+        var url = baseUrl + 'api/Traffic/GetTraffic';
+
+        httpService.get(
+            url,
+            callback.bind(this),
+            errorCallback ? errorCallback.bind(this) : null,
+            {
+                $ViewElement: $('#traffic_view'),
+                ErrorMessage: 'Error Fetching Traffic Data',
+                TitleErrorMessage: 'Error Fetching Traffic Data',
+                StatusMessage: 'Error Fetching Traffic Data'
+            });
     }
 });
