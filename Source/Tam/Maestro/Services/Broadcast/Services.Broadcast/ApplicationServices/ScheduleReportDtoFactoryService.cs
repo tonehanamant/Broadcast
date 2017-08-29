5f2d6ae4801599ce460d71d85de49eab3ac5abd9
mhohenshilt <CROSSMW\mhohenshilt>
2017-08-29 16:31:06 +0000

    BCOP-1797 fix weekly "spec status"
    git-tfs-id: [http://devvm01tfs03.telamericamedia.com:8080/tfs/CMWPA]$/TelAmerica/Releases/5.8/5.8.8;C20700

--- Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/ApplicationServices/ScheduleReportDtoFactoryService.cs
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/ApplicationServices/ScheduleReportDtoFactoryService.cs
@@ -681,7 +681,6 @@ namespace Services.Broadcast.ApplicationServices
                 };
                 weeklyData.ReportDataByWeek.Add(weeklyDto);
 
-                    
                 //In Spec
                 foreach (var detail in coreDataList)
                 {
@@ -694,6 +693,7 @@ namespace Services.Broadcast.ApplicationServices
                     if (orderedSpots == 0 && deliveredSpots == 0)
                         continue;
 
+                    var status = deliveredSpots > 0 ? "Match" : string.Empty;
                     var bvsReportData = new BvsReportData
                     {
                         Rank = detail.Rank,
@@ -709,7 +709,7 @@ namespace Services.Broadcast.ApplicationServices
                         SpotClearance = (double) deliveredSpots/(double) orderedSpots,
                         Status = 1, //in-spec
                         //@todo Fill this out
-                        SpecStatus = "Match",
+                        SpecStatus = status,
                     };
 
                     foreach (var audience in scheduleAudiences)
