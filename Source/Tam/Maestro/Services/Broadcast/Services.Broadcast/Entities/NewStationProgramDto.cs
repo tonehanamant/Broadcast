fc571c8581ea857c9f6664277e97e051c23d1b5e
Vinicios Henrique de Moura <vinicios.demoura@axispoint.com>
2017-09-08 16:43:13 +0000

    bcop-1848 change impressions from float to double to support Fe values like 1234567890
    git-tfs-id: [http://devvm01tfs03.telamericamedia.com:8080/tfs/CMWPA]$/TelAmerica/Broadcast/INT;C20838

---
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Entities/NewStationProgramDto.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using Tam.Maestro.Data.Entities.DataTransferObjects;
+
+namespace Services.Broadcast.Entities
+{
+    public class NewStationProgramDto
+    {
+        public int Id { get; set; }
+        public int StationCode { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public RatesFile.RateSourceType RateSource { get; set; }
+        public DaypartDto Airtime { get; set; }
+        public string Program { get; set; }
+        public List<LookupDto> Genres { get; set; }
+        public decimal? Rate15 { get; set; }
+        public decimal? Rate30 { get; set; }
+        public double Impressions { get; set; }
+        public double Rating { get; set; }
+        public List<FlightWeekDto> Flights { get; set; }
+        public string Flight { get; set; }
+        public DateTime FlightStartDate { get; set; }
+        public DateTime FlightEndDate { get; set; }
+        public List<StationProgramConflictChangeDto> Conflicts { get; set; }
+    }
+
+    public class StationProgramConflictChangeDto
+    {
+        public int Id { get; set; }
+        public DateTime FlightStartDate { get; set; }
+        public DateTime FlightEndDate { get; set; }
+        public List<FlightWeekDto> Flights { get; set; }
+    }
+}
