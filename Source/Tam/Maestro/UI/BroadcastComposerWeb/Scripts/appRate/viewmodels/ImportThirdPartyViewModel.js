6e284b9f25f1fe36f76a5c8b58b70da68f497db7
Greg Ussery <greg.ussery@axispoint.com>
2017-10-16 19:02:50 +0000

    BCOP: 1780 fix upload call
    git-tfs-id: [http://devvm01tfs03.telamericamedia.com:8080/tfs/CMWPA]$/Telamerica/Broadcast/OnDeck;C21415

--- Source/Tam/Maestro/UI/BroadcastComposerWeb/Scripts/appRate/viewmodels/ImportThirdPartyViewModel.js
+++ Source/Tam/Maestro/UI/BroadcastComposerWeb/Scripts/appRate/viewmodels/ImportThirdPartyViewModel.js
@@ -212,11 +212,11 @@ var ImportThirdPartyViewModel = function (controller) {
             fileData.FixedPrice = fixed;
             //console.log('uploadFile', JSON.stringify(fileData));
             console.log('uploadFile', fileData);
-            //controller.apiUploadInventoryFile(fileData,
-            //    function (data) {
-            //        $scope.showModal(false);
-            //    }
-            //);
+            controller.apiUploadInventoryFile(fileData,
+                function (data) {
+                    $scope.showModal(false);
+                }
+            );
         }
     };
 };
