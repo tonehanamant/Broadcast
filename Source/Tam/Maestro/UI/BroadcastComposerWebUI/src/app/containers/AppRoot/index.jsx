9f0c5150650fac80e4bd754dbe628d3c9dc7a8e8
jhayes <jhayes@CAD-PA-JHAYES>
2017-11-15 11:16:49 -0500

    Deployment Configure: Parallel /broadcastreact in app

--- Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/app/containers/AppRoot/index.jsx
+++ Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/app/containers/AppRoot/index.jsx
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import AppMain from 'Containers/AppMain';
 
 const AppRoot = () => (
-    <Route path="/test" component={AppMain} />
+    <Route path="/broadcastreact" component={AppMain} />
 );
 
 export default AppRoot;
