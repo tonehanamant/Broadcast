c007988053303f09f5ce17b4d5bfbd1623d0a6d7
mhohenshilt <mhohenshilt@crossmw.com>
2017-12-06 10:53:57 -0500

    BCOP-2264 remove partial uploads

--- Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Entities/InventoryFileProblem.cs
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Entities/InventoryFileProblem.cs
@@ -6,8 +6,6 @@ namespace Services.Broadcast.Entities
 
     public class InventoryFileProblem
     {
-        [IgnoreDataMember]
-        public bool IsWarning { get; set; }
         public string StationLetters { get; set; }
         public string ProgramName { get; set; }
         public string ProblemDescription { get; set; }
@@ -21,12 +19,6 @@ namespace Services.Broadcast.Entities
             ProblemDescription = description;
             ProgramName = null;
             StationLetters = null;
-        }
-
-        public void AddWarning(string description)
-        {
-            ProblemDescription = description;
-            IsWarning = true;
         }
     }
 }
