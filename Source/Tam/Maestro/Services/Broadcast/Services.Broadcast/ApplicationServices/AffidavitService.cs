890df7345d04d7048ce22d7cd81ac0ba7f6003e3
Mike Trzcianowski <mike.trzcianowski@axispoint.com>
2018-01-03 15:09:16 -0500

    BCOP-2354, BCOP-2316
    Added basic structure of API to AffidavitService call

---
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/ApplicationServices/AffidavitService.cs
@@ -0,0 +1,26 @@
+﻿using Common.Services.ApplicationServices;
+using Services.Broadcast.Entities;
+using System;
+
+namespace Services.Broadcast.ApplicationServices
+{
+    public interface IAffidavitService : IApplicationService
+    {
+
+        bool SaveAffidavit(AffadavitSaveRequest saveRequest);
+    }
+
+    public class AffidavitService : IAffidavitService
+    {
+
+        public bool SaveAffidavit(AffadavitSaveRequest saveRequest)
+        {
+            if (saveRequest == null)
+            {
+                throw new Exception("No affadavit data received.");
+            }
+
+            return true;
+        }
+    }
+}
