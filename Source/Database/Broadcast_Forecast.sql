2ee48bee1fc91903637ceb3f1b5074ec8fb86307
crossmw\kmai <kmai@crossmedia.tv>
2018-01-19 11:22:01 -0500

    Revert "Update sql script to raise an error"
    This reverts commit d0d387a144835997ac35f15bc124963330d11e10.

--- Source/Database/Broadcast_Forecast.sql
+++ Source/Database/Broadcast_Forecast.sql
@@ -1996,8 +1996,8 @@ BEGIN
 	END
 	ELSE
 	BEGIN
+		PRINT 'Incorrect Previous Database Version'
 		ROLLBACK TRANSACTION
-		RAISERROR('Incorrect Previous Database Version', 11, 1)
 	END
 
 END
@@ -2006,9 +2006,15 @@ GO
 IF(XACT_STATE() = -1)
 BEGIN
 	ROLLBACK TRANSACTION
-	RAISERROR('Database Update Failed. Transaction rolled back.', 11, 1)
+	PRINT 'Database Update Failed. Transaction rolled back.'
 END
 GO
+
+
+
+
+
+
 
 
 
