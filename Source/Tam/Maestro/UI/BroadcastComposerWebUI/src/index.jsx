29ec2f7f660400e9e216d33c6f290717a3c036fd
jhayes <jhayes@CAD-PA-JHAYES>
2018-01-22 14:58:24 -0500

    Add back element-closest to index.jsx

--- Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/index.jsx
+++ Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
+import 'Utils/element-closest'; // Element.closest polyfill
 
 import AppRoot from 'Containers/AppRoot';
 
