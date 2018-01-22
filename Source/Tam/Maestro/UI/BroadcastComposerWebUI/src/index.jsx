a780772c9795f25695feec801ae51a5a590b6ade
jhayes <jhayes@CAD-PA-JHAYES>
2018-01-22 14:06:50 -0500

    BroadcastComposerWebUI Minor Cleanup

--- Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/index.jsx
+++ Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
-import 'Utils/element-closest'; // Element.closest polyfill
 
 import AppRoot from 'Containers/AppRoot';
 
@@ -13,8 +12,10 @@ import configureStore, { history } from './index.store';
 import 'react-select/dist/react-select.css';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+// import 'antd/dist/antd.css';
 import './index.css';
 import './index.scss';
+import 'antd/dist/antd.css';
 
 
 require('./assets/icons/favicon.png');
