398619d20ce665cf80c4bdb979fb7084827be8e6
mhohenshilt <mhohenshilt@crossmw.com>
2018-02-22 10:19:24 -0500

    BCOP-2516 First Shot

---
+++ Source/Solutions/BroadcastComposerWeb/WWTVData.Service/WWTV.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Timers;
+
+namespace WWTVData.Service
+{
+    public interface IWWTV
+    {
+        void Start();
+        void Stop();
+        void CheckWWTVFiles(DateTime timeSignaled);
+    }
+    public class WWTV : IWWTV
+    {
+        public const int MILSEC_BETWEEN_CHECKS = 1000 * 5;
+        private DateTime _timeLastRun;
+
+        public int SecondsBetweenRuns { get; set; }
+
+        readonly Timer _Timer;
+        public WWTV()
+        {
+            SecondsBetweenRuns = 10 ;
+            _Timer = new Timer(MILSEC_BETWEEN_CHECKS) { AutoReset = true }; // once an hour
+            _Timer.Elapsed += _Timer_check_for_WWT_files;
+            _timeLastRun = DateTime.Now;
+        }
+        protected void _Timer_check_for_WWT_files(object sender, ElapsedEventArgs e)
+        {
+            _Timer.Stop();
+
+            var signalTime = e.SignalTime;
+
+            if (signalTime.AddSeconds(-SecondsBetweenRuns) >= _timeLastRun)
+            {
+
+                CheckWWTVFiles(_timeLastRun);
+
+                _timeLastRun = signalTime;
+            }
+            _Timer.Start();
+        }
+        public void Start() { _Timer.Start(); }
+        public void Stop() { _Timer.Stop(); }
+
+        public void CheckWWTVFiles(DateTime timeSignaled)
+        {
+            Console.WriteLine("Not Implemented at time " + timeSignaled.ToString("s"));
+        }
+    }
+}
---
+++ Source/Solutions/BroadcastComposerWeb/WWTVData.Service/WWTVDataServiceHost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Topshelf;
+
+namespace WWTVData.Service
+{
+    
+    class WWTVDataServiceHost
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0] == "-console")
+            {
+                (new WWTV()).CheckWWTVFiles(DateTime.Now);
+            }
+            else
+            {
+                var rc = HostFactory.Run(x =>
+                {
+                    x.Service<WWTV>(s =>
+                    {
+                        s.ConstructUsing(name => new WWTV());
+                        s.WhenStarted(tc => tc.Start());
+                        s.WhenStopped(tc => tc.Stop());
+                    });
+                    x.RunAsLocalSystem();
+
+                    x.SetDescription("_WWTV Data Service");
+                    x.SetDisplayName("_WWTVData.Service");
+                    x.SetServiceName("_WWTVData.Service");
+                });
+                var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+                Environment.ExitCode = exitCode;
+            }
+        }
+    }
+}
