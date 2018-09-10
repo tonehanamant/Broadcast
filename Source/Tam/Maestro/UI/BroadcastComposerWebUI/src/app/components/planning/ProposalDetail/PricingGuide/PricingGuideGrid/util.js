249e5bd8b9ce4882d08d53ba10e9dd66197f3dd4
gussery <gussery@axispoint.com>
2018-09-10 16:28:50 -0400

    BCOP3562-3606 remove overnight column; adjust data props and impresiion calculations; adjust renderers and add grey rendering

--- Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/app/components/planning/ProposalDetail/PricingGuide/PricingGuideGrid/util.js
+++ Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/app/components/planning/ProposalDetail/PricingGuide/PricingGuideGrid/util.js
@@ -29,14 +29,15 @@ const generateData = (markets) => {
       rowType: rowTypes.TITLE,
       AiringTime: `${market.MarketRank}. ${market.MarketName}`,
       Spots: market.TotalSpots,
-      Impressions: market.TotalImpressions,
-      StationImpressions: null, // market.StationImpressions,
-      OvernightImpressions: market.TotalOvernightImpressions,
+      Impressions: market.TotalImpressions ? market.TotalImpressions / 1000 : market.TotalImpressions,
+      StationImpressions: market.TotalStationImpressions ? market.TotalStationImpressions / 1000 : market.TotalStationImpressions,
+      // OvernightImpressions: market.TotalOvernightImpressions,
       Cost: market.TotalCost,
+      isMarket: true, // need for future use
     });
     market.Stations.forEach((station) => {
       // station TotalStationImpressions?
-      data.push({ rowType: rowTypes.SUB_TITLE, AiringTime: `${station.CallLetters} (${station.LegacyCallLetters})` });
+      data.push({ rowType: rowTypes.SUB_TITLE, AiringTime: `${station.CallLetters} (${station.LegacyCallLetters})`, isStation: true });
 
       station.Programs.forEach((program) => {
         data.push({
@@ -45,10 +46,11 @@ const generateData = (markets) => {
           Program: program.ProgramName,
           CPM: program.BlendedCpm,
           Spots: program.Spots,
-          Impressions: program.Impressions,
-          StationImpressions: null, // program.StationImpressions,
-          OvernightImpressions: program.OvernightImpressions,
+          Impressions: program.Impressions ? program.Impressions / 1000 : program.Impressions,
+          StationImpressions: program.StationImpressions ? program.StationImpressions / 1000 : program.StationImpressions,
+          // OvernightImpressions: program.OvernightImpressions,
           Cost: program.Cost,
+          isProgram: true, // need for future use
         });
       });
 
@@ -59,14 +61,32 @@ const generateData = (markets) => {
   return data;
 };
 
-const NumberCell = ({ value }) => {
-  if (isNil(value)) return '';
-  return value !== 0 ? numeral(value).format('0,0.[00]') : '-';
+const GreyDisplay = (value, isGrey) => {
+  const color = isGrey ? '#8f8f8f' : 'black';
+  return (<div style={{ color }}>{value}</div>);
 };
 
-const DollarCell = ({ value }) => {
+const NumberCell = ({ value, original }) => {
+  // console.log('number row', original);
   if (isNil(value)) return '';
-  return value !== 0 ? numeral(value).format('$0,0.[00]') : '-';
+  const retVal = value !== 0 ? numeral(value).format('0,0') : '-';
+  const inactive = original.isProgram ? (original.Spots === 0 || original.Impressions > 0) : false;
+  return GreyDisplay(retVal, inactive);
+};
+
+const ImpressionCell = ({ value, original }) => {
+  // console.log('impression row', original);
+  if (isNil(value)) return '';
+  const inactive = original.isProgram ? (original.Spots === 0 || original.Impressions > 0) : false;
+  const retVal = value !== 0 ? numeral(value).format('0,0.[000]') : '-';
+  return GreyDisplay(retVal, inactive);
+};
+
+const DollarCell = ({ value, original }) => {
+  if (isNil(value)) return '';
+  const retVal = value !== 0 ? numeral(value).format('$0,0.[00]') : '-';
+  const inactive = original.isProgram ? (original.Spots === 0 || original.Impressions > 0) : false;
+  return GreyDisplay(retVal, inactive);
 };
 
 const GroupingCell = ({ original: { rowType }, value }) => {
@@ -82,10 +102,12 @@ GroupingCell.propTypes = {
 const columns = [{
   Header: 'Airing Time',
   accessor: 'AiringTime',
+  minWidth: 150,
   Cell: GroupingCell,
 }, {
   Header: 'Program',
   accessor: 'Program',
+  minWidth: 200,
 }, {
   Header: 'CPM',
   accessor: 'CPM',
@@ -97,16 +119,16 @@ const columns = [{
 }, {
   Header: 'Impressions(OOO)',
   accessor: 'Impressions',
-  Cell: NumberCell,
+  Cell: ImpressionCell,
 }, {
   Header: 'Station Impressions',
   accessor: 'StationImpressions',
-  Cell: NumberCell,
-}, {
+  Cell: ImpressionCell,
+}, /* {
   Header: 'Overnight Impressions',
   accessor: 'OvernightImpressions',
   Cell: NumberCell,
-}, {
+}, */ {
   Header: 'Cost',
   accessor: 'Cost',
   Cell: DollarCell,
