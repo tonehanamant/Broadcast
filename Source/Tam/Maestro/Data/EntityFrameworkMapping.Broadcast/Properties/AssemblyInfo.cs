230a2a96ff35686671710038de2f0733c1c252d8
phurych <phurych@cadent.tv>
2018-10-29 14:18:41 -0400

    Reset scripts and increment version for 18.12.1 (December) release

--- Source/Tam/Maestro/Data/EntityFrameworkMapping.Broadcast/Properties/AssemblyInfo.cs
+++ Source/Tam/Maestro/Data/EntityFrameworkMapping.Broadcast/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.09.1")]
-[assembly: AssemblyFileVersion("18.09.1")]
+[assembly: AssemblyVersion("18.12.1")]
+[assembly: AssemblyFileVersion("18.12.1")]
 
 
 
