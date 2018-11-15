150f5b74c84d929ef2f00192ce588047aae4f712
Bill Tallitsch <btallitsch@crossmw.com>
2018-11-15 15:01:32 -0500

    Merging BCOP4004 into dev

--- Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/app/components/postPrePosting/DataGridContainer/index.js
+++ Source/Tam/Maestro/UI/BroadcastComposerWebUI/src/app/components/postPrePosting/DataGridContainer/index.js
@@ -10,20 +10,7 @@ import {
   deletePostPrePosting,
   getPostPrePostingFileEdit
 } from "Ducks/postPrePosting";
-import { Grid, Actions } from "react-redux-grid";
-import CustomPager from "Components/shared/CustomPager";
-import ContextMenuRow from "Components/shared/ContextMenuRow";
-import Sorter from "Utils/react-redux-grid-sorter";
-
-/* ////////////////////////////////// */
-/* // REACT-REDUX-GRID ACTIONS
-/* ////////////////////////////////// */
-const { MenuActions, SelectionActions, GridActions } = Actions;
-const { showMenu, hideMenu } = MenuActions;
-const { selectRow, deselectAll } = SelectionActions;
-const { doLocalSort } = GridActions;
-
-const stateKey = "gridPostPrePostingMain";
+import Table, { withGrid } from "Lib/react-table";
 
 /* ////////////////////////////////// */
 /* // MAPPING STATE AND DISPATCH
@@ -31,23 +18,16 @@ const stateKey = "gridPostPrePostingMain";
 const mapStateToProps = ({
   postPrePosting: { initialdata },
   postPrePosting: { post },
-  grid,
-  dataSource,
   menu
 }) => ({
-  // App
   initialdata,
   post,
-  // React-Redux-Grid
-  grid,
-  dataSource,
   menu
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      // App
       getPostPrePostingInitialData,
       getPostPrePosting,
       getPostPrePostingFiltered,
@@ -55,13 +35,7 @@ const mapDispatchToProps = dispatch =>
       toggleModal,
       deletePostPrePosting,
       getPostPrePostingFileEdit,
-      setOverlayLoading,
-      // React-Redux-Grid
-      showMenu,
-      hideMenu,
-      selectRow,
-      deselectAll,
-      doLocalSort
+      setOverlayLoading
     },
     dispatch
   );
@@ -78,51 +52,6 @@ export class DataGridContainer extends Component {
     this.contextMenuFileSettingsAction = this.contextMenuFileSettingsAction.bind(
       this
     );
-    this.deselectAll = this.deselectAll.bind(this);
-    this.selectRow = this.selectRow.bind(this);
-  }
-
-  /* ////////////////////////////////// */
-  /* LIFE-CYCLE METHODS */
-  /* ////////////////////////////////// */
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.post !== this.props.post) {
-      this.props.setOverlayLoading({
-        id: "gridPostPrePostingMain",
-        loading: true
-      });
-      // Evaluate if sort directions is set on column or default
-      // Sort dataSource using Sorter
-      setTimeout(() => {
-        const cols = this.props.grid
-          .get("gridPostPrePostingMain")
-          .get("columns");
-        let sortCol = cols.find(x => x.sortDirection);
-        if (!sortCol) sortCol = cols.find(x => x.defaultSortDirection);
-        if (sortCol) {
-          const datasource = this.props.dataSource.get(
-            "gridPostPrePostingMain"
-          );
-          const sorted = Sorter.sortBy(
-            sortCol.dataIndex,
-            sortCol.sortDirection || sortCol.defaultSortDirection,
-            datasource
-          );
-          this.props.doLocalSort({
-            data: sorted,
-            stateKey: "gridPostPrePostingMain"
-          });
-        }
-        this.props.setOverlayLoading({
-          id: "gridPostPrePostingMain",
-          loading: false
-        });
-      }, 0); // SET_DATA is completed, for dataSource
-
-      // Hide Context Menu (assumes visible)
-      this.props.hideMenu({ stateKey: "gridPostPrePostingMain" });
-    }
   }
 
   /* ////////////////////////////////// */
@@ -149,65 +78,36 @@ export class DataGridContainer extends Component {
   }
 
   /* ////////////////////////////////// */
-  /* // GRID ACTION METHOD BINDINGS
-  /* ////////////////////////////////// */
-  hideContextMenu(ref) {
-    this.props.hideMenu(ref);
-  }
-  showContextMenu(ref) {
-    this.props.showMenu(ref);
-  }
-
-  selectRow(rowId) {
-    this.deselectAll();
-    this.props.selectRow({ rowId, stateKey });
-  }
-  deselectAll() {
-    this.props.deselectAll({ stateKey });
-  }
-
-  /* ////////////////////////////////// */
   /* // COMPONENT RENDER FUNC
   /* ////////////////////////////////// */
   render() {
     /* ////////////////////////////////// */
-    /* // REACT-REDUX-GRID CONFIGURATION
+    /* // REACT-TABLE CONFIGURATION
     /* ////////////////////////////////// */
-
-    /* GRID RENDERERS */
-    // const renderers = {
-    //   uploadDate: ({ value, row }) => (
-    //     <span>{row.DisplayUploadDate}</span>
-    //   ),
-    //   modifiedDate: ({ value, row }) => (
-    //     <span>{row.DisplayModifiedDate}</span>
-    //   ),
-    // };
 
     /* GRID COLUMNS */
     const columns = [
       {
-        name: "File Name",
-        dataIndex: "FileName",
-        width: "20%"
+        Header: "File Name",
+        accessor: "FileName",
+        maxWidth: 400
       },
       {
-        name: "Demos",
-        dataIndex: "DisplayDemos",
-        width: "40%"
+        Header: "Demos",
+        accessor: "DisplayDemos",
+        maxWidth: 400
       },
       {
-        name: "Upload Date",
-        dataIndex: "UploadDate",
-        defaultSortDirection: "ASC",
-        width: "20%",
-        renderer: ({ row }) => <span>{row.DisplayUploadDate}</span>
+        Header: "Upload Date",
+        accessor: "DisplayUploadDate",
+        maxWidth: 200,
+        Cell: row => <span>{row.value}</span>
       },
       {
-        name: "Last Modified",
-        dataIndex: "ModifiedDate",
-        width: "20%",
-        renderer: ({ row }) => <span>{row.DisplayModifiedDate}</span>
+        Header: "Last Modified",
+        accessor: "DisplayModifiedDate",
+        maxWidth: 200,
+        Cell: row => <span>{row.value}</span>
       }
     ];
 
@@ -239,64 +139,18 @@ export class DataGridContainer extends Component {
       }
     ];
 
-    /* GRID PLGUINS */
-    const plugins = {
-      COLUMN_MANAGER: {
-        resizable: true,
-        moveable: false,
-        sortable: {
-          enabled: true,
-          method: "local"
-        }
-      },
-      EDITOR: {
-        type: "inline",
-        enabled: false
-      },
-      PAGER: {
-        enabled: false,
-        pagingType: "local",
-        pagerComponent: <CustomPager stateKey={stateKey} idProperty="Id" />
-      },
-      LOADER: {
-        enabled: false
-      },
-      SELECTION_MODEL: {
-        mode: "single",
-        enabled: true,
-        allowDeselect: true,
-        activeCls: "active",
-        selectionEvent: "singleclick"
-      },
-      ROW: {
-        enabled: true,
-        renderer: ({ cells, ...rowData }) => (
-          <ContextMenuRow
-            {...rowData}
-            menuItems={menuItems}
-            stateKey={stateKey}
-            beforeOpenMenu={this.selectRow}
-          >
-            {cells}
-          </ContextMenuRow>
-        )
-      }
-    };
-
-    /* GRID EVENT HANDLERS */
-    const events = {
-      HANDLE_BEFORE_SORT: () => {
-        this.deselectAll();
-      }
-    };
-
-    const grid = {
-      columns,
-      plugins,
-      events,
-      stateKey
-    };
-    return <Grid {...grid} data={this.props.post} store={this.context.store} />;
+    return (
+      <Table
+        data={this.props.post}
+        style={{ fontSize: "12px", marginBottom: "100px" }}
+        columns={columns}
+        contextMenu={{
+          isRender: true,
+          menuItems
+        }}
+        selection="single"
+      />
+    );
   }
 }
 
@@ -304,28 +158,13 @@ export class DataGridContainer extends Component {
 /* // PROPTYPES
 /* ////////////////////////////////// */
 DataGridContainer.propTypes = {
-  grid: PropTypes.object.isRequired,
-  dataSource: PropTypes.object.isRequired,
-  menu: PropTypes.object.isRequired,
   post: PropTypes.array.isRequired,
-
-  getPostPrePostingInitialData: PropTypes.func.isRequired,
-  getPostPrePosting: PropTypes.func.isRequired,
-  getPostPrePostingFiltered: PropTypes.func.isRequired,
   toggleModal: PropTypes.func.isRequired,
-  createAlert: PropTypes.func.isRequired,
   deletePostPrePosting: PropTypes.func.isRequired,
-  getPostPrePostingFileEdit: PropTypes.func.isRequired,
-  setOverlayLoading: PropTypes.func.isRequired,
-
-  showMenu: PropTypes.func.isRequired,
-  hideMenu: PropTypes.func.isRequired,
-  selectRow: PropTypes.func.isRequired,
-  deselectAll: PropTypes.func.isRequired,
-  doLocalSort: PropTypes.func.isRequired
+  getPostPrePostingFileEdit: PropTypes.func.isRequired
 };
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(DataGridContainer);
+)(withGrid(DataGridContainer));
