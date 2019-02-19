602450bb48c5188bd2a6f60c257a043c71bcdc0e
mhohenshilt <mhohenshilt@crossmw.com>
2019-02-19 16:49:17 -0500

    PRI-4396 2 book divide by zero error

--- Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Repositories/RatingForecastRepository.cs
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Repositories/RatingForecastRepository.cs
@@ -218,8 +218,8 @@ namespace Services.Broadcast.Repositories
                     {
                         Value = (char)PlaybackTypeConverter.ProposalPlaybackTypeToForecastPlaybackType(playbackType)
                     };
-                    //WriteTableSQLDebug(stationDetails);
                     var storedProcedureName = useDayByDayImpressions ? "usp_GetImpressionsForMultiplePrograms_TwoBooks" : "usp_GetImpressionsForMultiplePrograms_TwoBooks_Averages";
+//                    WriteTableSQLDebug(storedProcedureName, stationDetails,hutMediaMonth,shareMediaMonth, string.Join(",", uniqueRatingsAudiences), PlaybackTypeConverter.ProposalPlaybackTypeToForecastPlaybackType(playbackType).ToString());
 
                     return c.Database.SqlQuery<StationImpressions>(string.Format(@"EXEC [nsi].[{0}] @hut_media_month_id, @share_media_month_id, @demo, @ratings_request, @min_playback_type", storedProcedureName), hut, share, audienceId, ratingsRequest, minPlaybackType).ToList();
                 });
