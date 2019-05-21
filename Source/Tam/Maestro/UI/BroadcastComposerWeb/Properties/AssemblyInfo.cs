21796eff27491e203b5434bc637867697edef3dc
Peter Armington <parmington@crossmw.com>
2019-05-21 16:34:16 -0400

    Merged PR 3800: Increment to 19.07
    Increment to 19.07

--- Source/Tam/Maestro/UI/BroadcastComposerWeb/Properties/AssemblyInfo.cs
+++ Source/Tam/Maestro/UI/BroadcastComposerWeb/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("19.06.1")]
-[assembly: AssemblyFileVersion("19.06.1")]
+[assembly: AssemblyVersion("19.07.1")]
+[assembly: AssemblyFileVersion("19.07.1")]
 
 
 
