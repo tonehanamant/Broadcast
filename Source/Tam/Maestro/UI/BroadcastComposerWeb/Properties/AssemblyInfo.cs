f09c9363ade14b82a1257c6d8bf8131895b1b079
Peter Armington <parmington@crossmw.com>
2019-07-16 16:22:43 -0400

    Merged PR 4454: Increment version
    Increment version

--- Source/Tam/Maestro/UI/BroadcastComposerWeb/Properties/AssemblyInfo.cs
+++ Source/Tam/Maestro/UI/BroadcastComposerWeb/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("19.08.1")]
-[assembly: AssemblyFileVersion("19.08.1")]
+[assembly: AssemblyVersion("19.09.1")]
+[assembly: AssemblyFileVersion("19.09.1")]
 
 
 
