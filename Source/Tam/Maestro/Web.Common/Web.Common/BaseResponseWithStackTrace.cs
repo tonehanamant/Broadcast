f0960d5c9d0e778bf850d62bb4abd41269e71cda
Shaun Espiritu <sespiritu@crossmw.com>
2019-09-03 11:21:51 -0400

    Merged PR 5145: Revert "Merged PR 5132: The Web.Common project has been copied into the
    Revert "Merged PR 5132: The Web.Common project has been copied into the Broadcast system so that it can"
    
    This reverts commit bf7f95bcc428b359236e77ae98a0f2701ef655f0.

--- Source/Tam/Maestro/Web.Common/Web.Common/BaseResponseWithStackTrace.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tam.Maestro.Services.Cable.Entities;
-
-namespace Tam.Maestro.Web.Common
-{
-    public class BaseResponseWithStackTrace<T> : BaseResponse<T>
-    {
-        public string StackTrace { get; set; }
-    }
-}
