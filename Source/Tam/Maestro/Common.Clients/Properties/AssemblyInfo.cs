0c6de58e63f89f15e984a187d461d144179045b9
Peter Armington <parmington@crossmw.com>
2019-09-24 15:46:34 -0400

    Merged PR 5563: increment to 2019.11
    increment to 2019.11

--- Source/Tam/Maestro/Common.Clients/Properties/AssemblyInfo.cs
+++ Source/Tam/Maestro/Common.Clients/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("18.4.1.16")]
-[assembly: AssemblyVersion("18.4.1.17")]
-[assembly: AssemblyFileVersion("18.4.1.17")]
+[assembly: AssemblyVersion("19.11.1")]
+[assembly: AssemblyFileVersion("19.11.1")]
 
 
 
