0d917deced263928549feed95a2dec755bae9231
Mike Trzcianowski <mtrzcianowski@crossmw.com>
2020-05-27 10:50:28 -0400

    Merged PR 8679: updated from drop_existing to if exists
    updated from drop_existing to if exists

--- Source/Database/Broadcast_Forecast.sql
+++ Source/Database/Broadcast_Forecast.sql
@@ -50,6 +50,21 @@ GO
 
 /*************************************** START UPDATE SCRIPT *****************************************************/
 
+/*************************************** START BP1-227 *****************************************************/
+IF EXISTS (SELECT * FROM sys.indexes WHERE name='IX_uvw_market_codes_call_letters' AND object_id = OBJECT_ID('nsi.uvw_market_codes_call_letters'))
+BEGIN
+    DROP INDEX [IX_uvw_market_codes_call_letters] ON [nsi].[uvw_market_codes_call_letters] WITH ( ONLINE = OFF )
+END
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [IX_uvw_market_codes_call_letters] ON [nsi].[uvw_market_codes_call_letters]
+(
+    [media_month_id] ASC,
+    [legacy_call_letters] ASC,
+    [market_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [MediaMonthSmallIntScheme]([media_month_id])
+GO
+/*************************************** END BP1-227 *****************************************************/
 
 /*************************************** END UPDATE SCRIPT *******************************************************/
 
