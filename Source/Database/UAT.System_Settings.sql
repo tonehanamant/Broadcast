52cfc43049f14522761ed812e707cf9a64dd118f
Shaun Espiritu <sespiritu@crossmw.com>
2020-06-08 10:44:57 -0400

    Merged PR 8802: System Settings : Phase 1 - added files

---
+++ Source/Database/UAT.System_Settings.sql
@@ -0,0 +1,91 @@
+﻿---------------------------------------------------------------------------------------------------
+-- !!! If Executing from SQL Server Manager, please enable SQLCMD Mode!!! To enable option, select menu Query->Enable SQLCMD mode. --
+---------------------------------------------------------------------------------------------------
+-- All scripts should be written in a way that they can be run multiple times
+-- All features/bugs should be wrapped in comments indicating the start/end of the scripts
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+-- TFS Items:
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+SET NOEXEC OFF;
+SET NOCOUNT OFF;
+GO
+
+:on error exit --sqlcmd exit script on error
+GO
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+	BEGIN
+		PRINT N'SQLCMD mode must be enabled to successfully execute this script.To enable option, select menu Query->Enable SQLCMD mode';
+		SET NOCOUNT ON;
+		SET NOEXEC ON; -- this will not execute any queries. queries will be compiled only.
+	END
+GO
+
+SET XACT_ABORT ON -- Rollback transaction incase of error
+GO
+
+
+BEGIN
+	PRINT 'RUNNING SCRIPT IN LOCAL DATBASE'
+END
+GO
+
+BEGIN TRANSACTION
+
+CREATE TABLE #previous_version 
+( 
+	[version] VARCHAR(32) 
+)
+GO
+
+-- Only run this script when the schema is in the correct previous version
+INSERT INTO #previous_version
+		SELECT parameter_value 
+		FROM system_component_parameters 
+		WHERE parameter_key = 'SchemaVersion' 
+GO
+
+/*************************************** START UPDATE SCRIPT *****************************************************/
+
+
+
+/*************************************** END UPDATE SCRIPT *******************************************************/
+
+-- Update the Schema Version of the database to the current release version
+UPDATE system_component_parameters 
+SET parameter_value = '20.07.1' -- Current release version
+WHERE parameter_key = 'SchemaVersion'
+GO
+
+print 'Updated schema Version'
+
+IF(XACT_STATE() = 1)
+BEGIN 
+	
+	IF EXISTS (SELECT TOP 1 * 
+		FROM #previous_version 
+		WHERE [version] = '20.06.1' -- Previous release version
+		OR [version] = '20.07.1') -- Current release version
+	BEGIN
+		PRINT 'Database Successfully Updated'
+		COMMIT TRANSACTION
+		DROP TABLE #previous_version
+	END
+	ELSE
+	BEGIN
+		ROLLBACK TRANSACTION
+		RAISERROR('Incorrect Previous Database Version', 11, 1)
+	END
+
+END
+GO
+
+IF(XACT_STATE() = -1)
+BEGIN
+	ROLLBACK TRANSACTION
+	RAISERROR('Database Update Failed. Transaction rolled back.', 11, 1)
+END
+GO
