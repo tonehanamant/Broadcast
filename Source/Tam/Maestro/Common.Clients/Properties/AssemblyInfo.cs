821884ece8af5f4fbc6d913811658afb892f5ce8
Shubhra Goel <sgoel@crossmw.com>
2020-08-25 15:38:49 -0400

    Merged PR 9677: version increment for code freeze
    version increment for code freeze

--- Source/Tam/Maestro/Common.Clients/Properties/AssemblyInfo.cs
+++ Source/Tam/Maestro/Common.Clients/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("18.4.1.16")]
-[assembly: AssemblyVersion("20.09.1")]
-[assembly: AssemblyFileVersion("20.09.1")]
+[assembly: AssemblyVersion("20.10.1")]
+[assembly: AssemblyFileVersion("20.10.1")]
 
 
 
