357972b38e785642169def3a2559746666e73c49
Shaun Espiritu <sespiritu@crossmw.com>
2020-11-03 12:18:47 -0500

    Merged PR 10451: 20.11 - Advance version
    20.11 - Advance version

--- Source/Tam/Maestro/UI/BroadcastComposerWeb/Properties/AssemblyInfo.cs
+++ Source/Tam/Maestro/UI/BroadcastComposerWeb/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("20.10.1")]
-[assembly: AssemblyFileVersion("20.10.1")]
+[assembly: AssemblyVersion("20.11.1")]
+[assembly: AssemblyFileVersion("20.11.1")]
 
 
 
