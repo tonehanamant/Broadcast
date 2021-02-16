05edb17235433c8a1ac795f9245c04d943ccdad2
Shaun Espiritu <sespiritu@crossmw.com>
2021-02-16 14:53:04 -0500

    Merged PR 11156: BP-1551 : AAB - Integrate Broadcast BE to work with new AAB Service - ThrowError
    Throw an error when not able to get the agencies from the traffic source

--- Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Cache/TrafficApiCache.cs
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Cache/TrafficApiCache.cs
@@ -1,14 +1,13 @@
-﻿using Common.Services.Extensions;
+﻿using BroadcastLogging;
+using Common.Services.Extensions;
+using log4net;
 using Services.Broadcast.Clients;
 using Services.Broadcast.Entities;
+using Services.Broadcast.Entities.DTO;
 using System;
 using System.Collections.Generic;
 using System.Runtime.Caching;
-using Services.Broadcast.Entities.DTO;
 using Tam.Maestro.Services.Cable.SystemComponentParameters;
-using log4net;
-using BroadcastLogging;
-using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Services.Broadcast.Cache
 {
@@ -34,7 +33,7 @@ namespace Services.Broadcast.Cache
         private const string CACHE_NAME_AGENCIES = "Agencies";
         private const string CACHE_KEY_AGENCIES = "Agencies";
         private readonly MemoryCache _AgencyCache = new MemoryCache(CACHE_NAME_AGENCIES);
-        private List<AgencyDto> _Agencies { get; set; } = new List<AgencyDto>();
+        private List<AgencyDto> _Agencies { get; set; }
 
         private const string CACHE_NAME_AGENCY_ADVERTISERS = "AgencyAdvertiserCache";
         private readonly BaseMemoryCache<List<AdvertiserDto>> _AgencyAdvertisersCache = new BaseMemoryCache<List<AdvertiserDto>>(CACHE_NAME_AGENCY_ADVERTISERS);
@@ -68,9 +67,14 @@ namespace Services.Broadcast.Cache
 
         public List<AgencyDto> GetAgencies()
         {
-            if (_Agencies?.Any() != true)
+            if (_Agencies == null)
             {
                 BuildAgencyCache(null);
+            }
+
+            if (_Agencies == null)
+            {
+                throw new InvalidOperationException("Failed to get Agencies from the source.");
             }
             return _Agencies;
         }
@@ -106,22 +110,27 @@ namespace Services.Broadcast.Cache
 
         private void BuildAgencyCache(CacheEntryRemovedArguments args)
         {
+            List<AgencyDto> agencies = null;
+            var policy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(CACHE_ITEM_TTL_SECONDS),
+                RemovedCallback = BuildAgencyCache
+            };
+
             try
             {
-                var agencies = _TrafficApiClient.GetAgencies();
-                var policy = new CacheItemPolicy
-                {
-                    AbsoluteExpiration = DateTime.Now.AddSeconds(CACHE_ITEM_TTL_SECONDS),
-                    RemovedCallback = BuildAgencyCache
-                };
-
-                _AgencyCache.Add(CACHE_KEY_AGENCIES, agencies, policy);
+                agencies = _TrafficApiClient.GetAgencies();
                 _Agencies = agencies;
+                
             }
             catch (Exception e)
             {
                 _LogError("Error attempting to refresh the Agency cache from the AAB Traffic Api.", e);
+                agencies = new List<AgencyDto>();
+                // set this to null so we pick up the error downstream
+                _Agencies = null;
             }
+            _AgencyCache.Add(CACHE_KEY_AGENCIES, agencies, policy);
         }
     }
 }
