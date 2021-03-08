5eaf8fe253af6553e94ffc3bd2e2777f1cdf1634
Shaun Espiritu <sespiritu@crossmw.com>
2021-03-08 13:00:06 -0500

    Merged PR 11316: BP-2019 : Campaigns Quarters dropdown displays data for all plan versions
    CR Story : https://jira.crossmw.com/browse/BP-2078

--- Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/ReportGenerators/ProgramMapping/UnmappedProgramsReportGenerator.cs
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/ReportGenerators/ProgramMapping/UnmappedProgramsReportGenerator.cs
@@ -2,6 +2,7 @@
 using Services.Broadcast.ApplicationServices.Inventory.ProgramMapping.Entities;
 using Services.Broadcast.Entities;
 using Services.Broadcast.Entities.ProgramMapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,6 +13,7 @@ namespace Services.Broadcast.ReportGenerators.ProgramMapping
         private const string CELL_A1_HEADER = "Rate Card Program Name";
         private const string CELL_B1_HEADER = "Matched Program Name";
         private const string CELL_C1_HEADER = "Matched Program Genre";
+        private const string CELL_D1_HEADER = "Confidence %";
         private const string TAB_NAME = "Unmapped Programs";
         private const int DATA_START_COLUMN = 1;
         private const int DATA_START_ROW = 2;
@@ -55,12 +57,23 @@ namespace Services.Broadcast.ReportGenerators.ProgramMapping
             worksheet.Cells["C1"].Value = CELL_C1_HEADER;
             worksheet.Column(3).Width = 50;
             worksheet.Cells["C1"].Style.Font.Bold = true;
+
+            worksheet.Cells["D1"].Value = CELL_D1_HEADER;
+            worksheet.Column(3).Width = 50;
+            worksheet.Cells["D1"].Style.Font.Bold = true;
         }
 
         private void _PopulateData(ExcelWorksheet worksheet, List<UnmappedProgram> programs)
         {
-            var tableData = programs.OrderByDescending(p => p.MatchConfidence).Select(p => new object[] { p.OriginalName, p.MatchedName, p.Genre });
+            var tableData = programs.OrderByDescending(p => p.MatchConfidence).Select(p => new object[] { p.OriginalName, p.MatchedName, p.Genre, _GetMatchConfidenceString(p.MatchConfidence)});
             worksheet.Cells[DATA_START_ROW, DATA_START_COLUMN].LoadFromArrays(tableData);
+        }
+
+        private string _GetMatchConfidenceString(float confidence)
+        {
+            var val = Math.Round((confidence * 100.0), 3);
+            var stringVal = $"{val}%";
+            return stringVal;
         }
     }
 }
