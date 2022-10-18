c63c8e210fea20372fdb4bd3fc98265220301da5
Stephen DeFusco <sdefusco@crossmw.com>
2022-10-18 15:18:12 +0000

    Merged PR 17609: Added default value
    Added default value

--- Source/Database/stratadw/dbo/Tables/estimates.sql
+++ Source/Database/stratadw/dbo/Tables/estimates.sql
@@ -5,7 +5,7 @@
     [description]       VARCHAR (4096) NOT NULL,
     [estimate_group]    VARCHAR (1024) NOT NULL,
     [product_name]      VARCHAR (256)  NOT NULL,
-    [estimate_code]     VARCHAR(64) NOT NULL, 
+    [estimate_code]     VARCHAR(64) NOT NULL DEFAULT '', 
     [plan_id]           INT NULL, 
     CONSTRAINT [PK_estimates] PRIMARY KEY CLUSTERED ([estimate_id] ASC)
 );
