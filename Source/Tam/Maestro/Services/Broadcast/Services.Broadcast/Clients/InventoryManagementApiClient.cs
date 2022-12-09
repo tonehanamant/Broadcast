9fde47f661018dc8fa2d489a732cb0fa2dd644d1
Pankaj Skula <pskula@cadent.tv>
2022-12-09 14:53:57 +0000

    Merged PR 18828: BP-5653:BE: NFR - BE - Inventory MS : Create the client
    Inventory MS : Create the client

---
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Clients/InventoryManagementApiClient.cs
@@ -0,0 +1,50 @@
+﻿using Services.Broadcast.Entities;
+using Services.Broadcast.Entities.Locking;
+using Services.Broadcast.Extensions;
+using Services.Broadcast.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Services.Broadcast.Clients
+{
+    public interface IInventoryManagementApiClient
+    {
+       
+    }
+    public class InventoryManagementApiClient : CadentSecuredClientBase, IInventoryManagementApiClient
+    {
+        public InventoryManagementApiClient(IApiTokenManager apiTokenManager,
+                 IFeatureToggleHelper featureToggleHelper, IConfigurationSettingsHelper configurationSettingsHelper)
+             : base(apiTokenManager, featureToggleHelper, configurationSettingsHelper)
+        {
+        }       
+        private async Task<HttpClient> _GetSecureHttpClientAsync()
+        {
+            var apiBaseUrl = _GetApiUrl();
+            var applicationId = _GetGeneralLockingApplicationId();
+            var appName = _GetGeneralLockingApiAppName();
+            var client = await _GetSecureHttpClientAsync(apiBaseUrl, applicationId, appName);
+            client.Timeout = new TimeSpan(2, 0, 0);
+            return client;
+        }
+
+        private string _GetApiUrl()
+        {
+            var apiUrl = _ConfigurationSettingsHelper.GetConfigValue<string>(InventoryManagementApiConfigKeys.ApiBaseUrl);
+
+            return apiUrl;
+        }
+        private string _GetGeneralLockingApplicationId()
+        {
+            var applicationId = _ConfigurationSettingsHelper.GetConfigValue<string>(InventoryManagementApiConfigKeys.ApplicationId);
+            return applicationId;
+        }
+        private string _GetGeneralLockingApiAppName()
+        {
+            var appName = _ConfigurationSettingsHelper.GetConfigValue<string>(InventoryManagementApiConfigKeys.AppName);
+            return appName;
+        }
+    }
+}
---
+++ Source/Tam/Maestro/Services/Broadcast/Services.Broadcast/Clients/InventoryManagementApiConfigKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Broadcast.Clients
+{
+    public class InventoryManagementApiConfigKeys
+    {
+        public static readonly string ApiBaseUrl = "InventoryManagementApi:ApiBaseUrl";
+        public static readonly string AppName = "InventoryManagementApi:AppName";
+        public static readonly string ApplicationId = "InventoryManagementApi:ApplicationId";
+    }
+}
